d9337f7e9dc5d985b4c8044cc9d7c90be18b14ae
Zayyad Butt <75773764+Zayyad-Butt@users.noreply.github.com>
2022-05-29 12:28:19 +0500

    Login and signup page functionalities added

---
+++ MVC_IS/MVC_IS/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVC_IS.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ MVC_IS/MVC_IS/ViewModels/LoginViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_IS.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [StringLength(20)]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
